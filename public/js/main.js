5b9e1cf37f11ae19958fac5a6058ea5a5201a598
Katie Brown <katieleebrown19@gmail.com>
2022-09-15 15:50:14 -0400

    map search for new orgs functional

--- public/js/main.js
+++ public/js/main.js
@@ -71,8 +71,91 @@ function partnerMuseumFormDisplay() {
     orgDetailsForm.classList.remove('hidden')
 }
 
-// For Search API - this displays correct divs. API call and subsequent display is in index.ts
+// For Google Find Place API
 function showMapDetails() {
     mapContainer.classList.remove('hidden')
     orgDetailsForm.classList.remove('hidden')
+    console.log('button clicked');
+
+    var search = document.getElementById('orgNameInput').value;
+
+    var request = {
+        query: search,
+        fields: ["name", "geometry", "place_id", "formatted_address"],
+    };
+
+    service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+            console.log(results);
+
+            //Set Org Name & Details
+            console.log(`orgName = ${results[0].name}`)
+            orgNameForm.innerText = results[0].name;
+            orgAddressForm.innerText = results[0].formatted_address;
+
+            // store Place_Id for later searches in local storage
+            var searchId = results[0].place_id;
+            localStorage.setItem('organization', searchId);
+            map.setCenter(results[0].geometry.location);
+        }
+    });
 }
+
+// Compiled TS for Google Maps
+// Object.defineProperty(exports, "__esModule", { value: true });
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+    console.log('map is loading');
+
+    var raleigh = new google.maps.LatLng(35.7796, 78.6382);
+
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: raleigh,
+        zoom: 15,
+    });
+
+    var request = {
+        query: "Museum of Natural Sciences",
+        fields: ["name", "geometry", "place_id", "formatted_address"],
+    };
+
+    console.log('request places')
+
+    service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+                console.log('marker created')
+            }
+            map.setCenter(results[0].geometry.location);
+            console.log('center set')
+        }
+    });
+}
+
+function createMarker(place) {
+    if (!place.geometry || !place.geometry.location)
+        return;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+    });
+    google.maps.event.addListener(marker, "click", function () {
+        infowindow.setContent(place.name || "");
+        infowindow.open(map);
+    });
+}
+
+window.initMap = initMap;
