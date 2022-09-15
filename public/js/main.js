33b8dc6a2cdcda8f354200bdfceb1091f8f4fb08
Katie Brown <katieleebrown19@gmail.com>
2022-09-15 12:42:26 -0400

    form card functionality for partner orgs

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,18 @@
 const deleteBtn = document.querySelectorAll('.del')
 const orgSearch = document.getElementById('organizationSearch')
 const pickMuseumList = document.getElementById('chooseMuseum')
+const orgNameForm = document.getElementById('displayOrgNameForm')
+const orgAddressForm = document.getElementById('displayOrgAddressForm')
+const addOrgNote = document.getElementById('museumSearchTag')
+const orgDetailsForm = document.getElementById('selectedOrgInfoForm')
+const mapContainer = document.getElementById('mapContainer')
 
 // Event Listeners
 Array.from(deleteBtn).forEach((el) => {
     el.addEventListener('click', deleteTodo)
 })
 pickMuseumList.addEventListener('click', displaySearch)
+
 
 // Delete Membership Function
 async function deleteTodo() {
@@ -28,11 +34,34 @@ async function deleteTodo() {
     }
 }
 
-// Display Organization Search
+// Display Organization Search or Update Display for Partner Museum
 function displaySearch() {
     if (pickMuseumList.selectedIndex > 0) {
         if ('other' === pickMuseumList.options[pickMuseumList.selectedIndex].value) {
             orgSearch.classList.remove('hidden')
+            mapContainer.classList.remove('hidden')
+        } else if ('caryChildrensMuseum' === pickMuseumList.options[pickMuseumList.selectedIndex].value) {
+            orgNameForm.innerText = `Cary Children's Museum`;
+            orgAddressForm.innerText = `100 N Main St, Cary NC 27516`
+            partnerMuseumFormDisplay()
+        } else if ('museumOfClimateSciences' === pickMuseumList.options[pickMuseumList.selectedIndex].value) {
+            orgNameForm.innerText = `Museum of Climate Sciences`;
+            orgAddressForm.innerText = `500 Sunshine Ln, Raleigh, NC 27202`
+            partnerMuseumFormDisplay()
+        } else if ('cityOfOaks' === pickMuseumList.options[pickMuseumList.selectedIndex].value) {
+            orgNameForm.innerText = `City of Oaks Historical Center`;
+            orgAddressForm.innerText = `1587 Sir Walter Ln, Raleigh, NC 27601`
+            partnerMuseumFormDisplay()
+        } else if ('ncFarmPark' === pickMuseumList.options[pickMuseumList.selectedIndex].value) {
+            orgNameForm.innerText = `North Carolina Farm Park & Zoo`;
+            orgAddressForm.innerText = `186 Cattlebrush Rd. Durham, NC 27606`
+            partnerMuseumFormDisplay()
         }
     }
+}
+
+function partnerMuseumFormDisplay() {
+    addOrgNote.classList.add('hidden')
+    mapContainer.classList.add('hidden')
+    orgDetailsForm.classList.remove('hidden')
 }
