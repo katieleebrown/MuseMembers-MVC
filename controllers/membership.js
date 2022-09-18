8c702d2b95fb7c3ca758560a96cf16d3158a11c7
Katie Brown <katieleebrown19@gmail.com>
2022-09-18 13:48:49 -0400

    card badges for expired/expiring soon

--- controllers/membership.js
+++ controllers/membership.js
@@ -4,7 +4,16 @@ module.exports = {
     getMemberships: async (req, res) => {
         console.log(req.user)
         try {
+            let currentDate = new Date()
+            let oneMonth = new Date()
+            oneMonth = oneMonth.setMonth(oneMonth.getMonth() + 1)
+            console.log(currentDate, oneMonth)
             const membershipCards = await Membership.find({ userId: req.user.id })
+            // need to forEach through membershipCards to add expired true/false and expiring soon true/false on render
+            membershipCards.forEach(card => {
+                card.expired = (new Date(card.expirationDate).getTime() < currentDate.getTime()) ? true : false;
+                card.expiringSoon = (new Date(card.expirationDate).getTime() < new Date(oneMonth).getTime()) ? true : false;
+            })
             // const itemsLeft = await Todo.countDocuments({ userId: req.user.id, completed: false })
             res.render('dashboard.ejs', { memberships: membershipCards }) //, left: itemsLeft, user: req.user
         } catch (err) {
@@ -13,16 +22,12 @@ module.exports = {
     },
     createMembership: async (req, res) => {
         // Probably need an if statement to determine what should be used for the museumName sent to the DB. Should pull place_id from localstorage if applicable, and maybe other data too?
-        let currentDate = new Date()
-        let expiredStatus = req.body.expiration <= currentDate.toDateString() ? true : false
-
         try {
             await Membership.create({
                 museumName: req.body.chooseMuseum,
                 maxGuests: req.body.maxGuests,
                 expirationDate: new Date(req.body.expiration).toDateString(),
                 userId: req.user.id,
-                expiresSoon: expiredStatus
             })
             console.log('Membership has been added!')
             res.redirect('/membership')
